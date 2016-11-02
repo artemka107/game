9d1d201d86b8233b15485008817b1adbf7ff3d26
artemka107 <artem-ivanec@mail.ru>
2016-11-02 15:04:10 +0300

    первый коммит

---
+++ src/js/check.js
@@ -0,0 +1,66 @@
+"use strict";
+
+function getMessage(a, b) {
+  if (typeof a === "boolean") {
+    if (a) {
+      return "Я попал в " + b;
+    } else {
+      return "Я никуда не попал";
+    }
+  }
+
+  if (typeof a === "number") {
+    return "Я прыгнул на " + a * 100 + " сантиметров";
+  }
+
+  if (Array.isArray(a) && !Array.isArray(b)) {
+    var numberOfSteps = 0;
+    for (var i = 0; i < a.length; i++) {
+      numberOfSteps += a[i];
+    }
+    return "Я прошёл " + numberOfSteps + " шагов"
+  }
+
+  if (Array.isArray(a) && Array.isArray(b)) {
+    var distance = 0;
+    for (var i = 0; i < a.length; i++) {
+      distance += a[i] * b[i];
+    }
+    return "Я прошёл " + distance + " метров";
+  }
+  return "переданные данные не корректны";
+}
+
+// 'use strict'
+//
+// function getMessage(a, b){
+//   if (typeof a == 'boolean') {
+//     if (a) {
+//       return "Я попал в " + b;
+//     } else {
+//       return "Я никуда не попал";
+//     }
+//   }
+//
+//   if (typeof a == 'number') {
+//     return "Я прыгнул на " + a * 100 + " сантиметров";
+//   }
+//
+//   if (Array.isArray(a) && !Array.isArray(b)) {
+//     var numberOfSteps = 0;
+//     for (var i = 0; i < a.length; i++) {
+//       numberOfSteps += a[i];
+//     }
+//       return "Я прошёл " + numberOfSteps + " шагов";
+//   }
+//
+//   if (Array.isArray(a) && Array.isArray(b)) {
+//     var distancePath = 0;
+//     for (var i = 0; i < a.length; i++){
+//       distancePath += a[i] * b[i];
+//     }
+//     return "Я прошёл " + distancePath + " метров";
+//   }
+//
+//   return "Переданы некорректные данные";
+// }
